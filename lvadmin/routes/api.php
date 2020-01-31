c696b058ca248ea256150ca35ea73908618052df
sitaramkdks <44518405+sitaramkdks@users.noreply.github.com>
2020-01-31 20:42:17 +0530

    added forms module

---
+++ lvadmin/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+ 
+
+ 
+
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::get('details', 'API\UserController@details');
+
+Route::get('logout', 'API\UserController@logoutApi');
+Route::get('getAllUsers', 'API\UserController@getAllUsers');
+
+});
